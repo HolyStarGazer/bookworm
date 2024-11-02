851a865fcc86e04faacdf29924da259caab52220
stapp425 <stapp425@gmail.com>
2024-11-02 14:21:48 -0500

    added books_authors sample data

--- backend/middleware/connect.ts
+++ backend/middleware/connect.ts
@@ -8,8 +8,16 @@ const connectToDB = async (_: Request, res: Response) => {
     console.log("Sucessfully connected to database.")
 
     try {
-      const results = await pool.query("SELECT * FROM public.samples")
-      res.json({ results: results.rows })
+      const query = `
+        SELECT books.title, JSON_AGG(CONCAT(authors.first_name, ' ', authors.last_name)) AS authors FROM books_authors
+        JOIN books ON books_authors.book_id = books.id
+        JOIN authors ON books_authors.author_id = authors.id
+        GROUP BY books.title
+        ORDER BY books.title;
+      `
+      
+      const { rows } = await pool.query(query)
+      res.json(rows)
     } catch (err: any) {
       res.status(500).json({ ERROR: err.stack })
     } finally {
