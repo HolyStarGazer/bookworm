1cd8b61039e40d6c60c85ebad7a40ac1c92bb633
stapp425 <stapp425@gmail.com>
2024-11-10 18:29:32 -0600

    updated book insert functionality in the backend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.retrofit2.gson)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.recyclerview)
--- backend/middleware/bookwormAPI.ts
+++ backend/middleware/bookwormAPI.ts
@@ -1,24 +1,32 @@
-import { type Request, type Response, type NextFunction } from "express"
+import { type Request, type Response } from "express"
+import { type Book } from "../util/types/book"
+import { type Author } from "../util/types/author"
+import { type Tag } from "../util/types/tag"
 import pool from "../util/pool"
 
-async function fetchGraphQL(operationsDoc: string, operationName: string, variables: Record<string, any>) {
-  const result = await fetch(
-    "https://api.hardcover.app/v1/graphql",
-    {
-      method: "POST",
-      headers: {
-        'content-Type': 'application/json',
-        'authorization': process.env.BEARER_TOKEN
-      } as HeadersInit,
-      body: JSON.stringify({
-        query: operationsDoc,
-        variables: variables,
-        operationName: operationName
-      })
-    }
-  )
 
-  return await result.json()
+async function fetchGraphQL(operationsDoc: string, operationName: string, variables: Record<string, unknown>) {
+  try {
+    const result = await fetch(
+      "https://api.hardcover.app/v1/graphql",
+      {
+        method: "POST",
+        headers: {
+          'content-Type': 'application/json',
+          'authorization': process.env.BEARER_TOKEN
+        } as HeadersInit,
+        body: JSON.stringify({
+          query: operationsDoc,
+          variables: variables,
+          operationName: operationName
+        })
+      }
+    )
+  
+    return await result.json()
+  } catch (err: any) {
+    console.error(err.message)
+  }
 }
 
 const operationsDoc = `
@@ -29,9 +37,8 @@ const operationsDoc = `
     }
   }
   
-  query dataBooks {
-    books(order_by: {id: asc}) {
-      id
+  query dataBooks($booksLimit: Int!, $booksOffset: Int!) {
+    books(order_by: {id: asc}, limit: $booksLimit, offset: $booksOffset) {
       title
       subtitle
       book_category_id
@@ -79,6 +86,7 @@ const operationsDoc = `
         isbn_13
         publisher_id
       }
+      id
     }
   }
   
@@ -89,19 +97,15 @@ const operationsDoc = `
     }
   }
   
-  query dataTags {
-    tag_categories {
+  query dataTags($tagId: bigint) {
+    tags(where: {id: {_eq: $tagId}}) {
       id
-      category
-      tags(order_by: {id: asc}) {
-        tag
-        id
-      }
+      tag
     }
   }
   
-  query dataAuthors {
-    authors(order_by: {id: asc}) {
+  query dataAuthors($authorId: Int!) {
+    authors(order_by: {id: asc}, where: {id: {_eq: $authorId}}) {
       id
       name
       title
@@ -110,8 +114,8 @@ const operationsDoc = `
     }
   }
   
-  query dataPublishers {
-    publishers(order_by: {id: asc}) {
+  query dataPublishers($publisherId: bigint) {
+    publishers(order_by: {id: asc}, where: {id: {_eq: $publisherId}}) {
       id
       name
     }
@@ -131,32 +135,155 @@ const operationsDoc = `
 `
 
 const fetchBookCategories = async (_: Request, res: Response) => {
-  const instance = await pool.connect()
-  
   try {
-    const { data } = await fetchGraphQL(operationsDoc, "dataBookCategories", {})
-    const categories: Array<{ name: string, id: number }> = data.book_categories
+    const instance = await pool.connect()
+  
+    try {
+      const { data } = await fetchGraphQL(operationsDoc, "dataBookCategories", {})
+      const categories: Array<{ name: string, id: number }> = data.book_categories
 
-    if(!categories)
-      throw new Error("book_categories array does not exist")
+      if(!categories)
+        throw new Error("book_categories array does not exist")
 
-    const query = `INSERT INTO categories VALUES ${categories.map((_, i) => `($${i + 1})`).join(", ")}`
+      const query = `INSERT INTO categories VALUES ${categories.map((_, i) => `($${i + 1})`).join(", ")}`
+      const values = categories.map(c => c.name)
 
-    const values = categories.map(c => c.name)
-    await instance.query(query, values)
+      await instance.query(query, values)
 
-    res.json({ SUCCESS: "Insert successful!" })
+      res.json({ SUCCESS: "Insert successful!" })
+    } catch (err: any) {
+      res.status(500).json({ ERROR: err.message })
+    } finally {
+      instance.release()
+    }
   } catch (err: any) {
     res.status(500).json({ ERROR: err.message })
-  } finally {
-    instance.release()
   }
 }
 
 const fetchBooks = async (_: Request, res: Response) => {
   try {
-    const { data } = await fetchGraphQL(operationsDoc, "dataBooks", {})
-    res.json({ data })
+    const instance = await pool.connect()
+
+    try {
+      const { data: { books } } = await fetchGraphQL(operationsDoc, "dataBooks", { booksLimit: 100, booksOffset: 0 })
+
+      const authorsArr: Array<number> = []
+      const taggingsArr: Array<number> = []
+      const publishersSet: Array<number> = []
+
+      for (const book of books) {        
+        const { contributions, taggings } = book
+        const publishers: Array<number | null | undefined> = [
+          book.default_physical_edition?.publisher_id,
+          book.default_ebook_edition?.publisher_id,
+          book.default_cover_edition?.publisher_id,
+          book.default_audio_edition?.publisher_id
+        ]
+
+        for (const c of contributions)  {
+          const id = c.author_id
+          
+          if (!authorsArr.includes(id)) {
+            const { data: { authors } } = await fetchGraphQL(operationsDoc, "dataAuthors", { authorId: id })
+            const foundAuthor: Author = authors[0]
+
+            authorsArr.push(id)
+
+            const authorQuery = `
+              INSERT INTO authors(name, title, books_count, users_count, id)
+              VALUES ($1, $2, $3, $4, $5)
+              ON CONFLICT (id) DO NOTHING
+            `
+            
+            const authorValues = [foundAuthor.name, foundAuthor.title, foundAuthor.books_count, foundAuthor.users_count, foundAuthor.id]
+            
+            await instance.query(authorQuery, authorValues)
+          }
+        }
+        
+        for (const t of taggings) {
+          const id = t.tag_id
+          
+          if (!taggingsArr.includes(id)) {
+            const { data: { tags } } = await fetchGraphQL(operationsDoc, "dataTags", { tagId: id })
+            const foundTag: Tag = tags[0]    
+            
+            taggingsArr.push(id)
+
+            const tagQuery = `
+              INSERT INTO tags(tag, id)
+              VALUES ($1, $2)
+              ON CONFLICT (id) DO NOTHING
+            `
+
+            const tagValues = [foundTag.tag, foundTag.id]
+            
+            await instance.query(tagQuery, tagValues)
+          }
+        }
+
+
+        for (const p of publishers) {
+          if (p && !publishersSet.includes(p)) {
+            const { data: { publishers } } = await fetchGraphQL(operationsDoc, "dataPublishers", { publisherId: p })
+            const foundPublisher = publishers[0]
+
+            publishersSet.push(p)
+
+            const publisherQuery = `
+              INSERT INTO publishers(name, id)
+              VALUES ($1, $2)
+            `
+            const publisherValues = [foundPublisher.name, foundPublisher.id]
+
+            await instance.query(publisherQuery, publisherValues)
+          }
+        }
+        
+        const booksQuery = `INSERT INTO 
+          books(
+            id, title,
+            subtitle,
+            book_category_id, book_status_id,
+            contributions,
+            description,
+            image, pages,
+            rating, ratings_count, ratings_distribution,
+            release_date, release_year,
+            reviews_count, users_read_count,
+            taggings,
+            default_physical_edition, default_ebook_edition, default_cover_edition, default_audio_edition
+          ) VALUES \n(${Array.from({ length: 21 }, (_, i) => `$${i + 1}`).join(",\n")})
+        `
+
+        const booksValues = [
+          book.id, book.title,
+          book.subtitle,
+          book.book_category_id, book.book_status_id,
+          book.contributions ? JSON.stringify(book.contributions) : null,
+          book.description,
+          book.image ? JSON.stringify(book.image) : null, 
+          book.pages, book.rating, book.ratings_count, 
+          book.ratings_distribution ? JSON.stringify(book.ratings_distribution) : null,
+          book.release_date, book.release_year,
+          book.reviews_count, book.users_read_count,
+          book.taggings ? JSON.stringify(book.taggings) : null,
+          book.default_physical_edition ? JSON.stringify(book.default_physical_edition) : null, 
+          book.default_ebook_edition ? JSON.stringify(book.default_ebook_edition) : null,
+          book.default_cover_edition ? JSON.stringify(book.default_cover_edition) : null,
+          book.default_audio_edition ? JSON.stringify(book.default_audio_edition) : null
+        ]
+
+        await instance.query(booksQuery, booksValues)
+      }
+
+      res.json({ SUCCESS: "All inserts successful!" })
+    } catch (err: any) {
+      res.status(500).json({ ERROR: err.stack })
+    } finally {
+      instance.release()
+    }
   } catch (err: any) {
     res.status(500).json({ ERROR: err.message })
   }
@@ -164,38 +291,103 @@ const fetchBooks = async (_: Request, res: Response) => {
 
 const fetchStatuses = async (_: Request, res: Response) => {
   try {
-    const { data } = await fetchGraphQL(operationsDoc, "dataStatuses", {})
-    res.json({ data })
+    const instance = await pool.connect()
+    
+    try {
+      const { data } = await fetchGraphQL(operationsDoc, "dataStatuses", {})
+      const bookStatuses: Array<{ name: string, id: number }> = data.book_statuses
+      
+      if (!bookStatuses)
+        throw new Error("book_statuses array does not exist")
+
+      const query = `INSERT INTO book_statuses(name, id) VALUES ${bookStatuses.map((_, i) => `($${i * 2 + 1}, $${i * 2 + 2})`).join(", ")}`
+      const values = bookStatuses.flatMap(s => [s.name, s.id])
+
+      await instance.query(query, values)
+      res.json({ SUCCESS: "Insert successful!" })
+    } catch (err: any) {
+      res.status(500).json({ ERROR: err.message })
+    } finally {
+      instance.release()
+    }
   } catch (err: any) {
-    res.status(500).json({ ERROR: err.message })
+    console.error(err.message)
   }
 }
 
 const fetchTags = async (_: Request, res: Response) => {
   try {
-    const { data } = await fetchGraphQL(operationsDoc, "dataTags", {})
-    res.json({ data })
+    const instance = await pool.connect()
+
+    try {
+      const { data } = await fetchGraphQL(operationsDoc, "dataTags", {})
+      const tags = data.
+      
+      res.json({ data })
+    } catch (err: any) {
+      res.status(500).json({ ERROR: err.message })
+    } finally {
+      instance.release()
+    }
   } catch (err: any) {
-    res.status(500).json({ ERROR: err.message })
+    console.error(err.message)
   }
+  
 } 
 
 const fetchAuthors = async (_: Request, res: Response) => {
   try {
-    const { data } = await fetchGraphQL(operationsDoc, "dataAuthors", {})
-    res.json({ data })
+    const instance = await pool.connect()
+
+    try {
+      const { data } = await fetchGraphQL(operationsDoc, "dataAuthors", {})
+      
+      if (!data.authors)
+        throw new Error("authors array does not exist")
+
+      const authors: Array<Author> = data.authors
+
+      const query = `INSERT INTO authors(name, title, books_count, users_count, id) VALUES\n${authors.map((_, i) => `(${Array.from({ length: 5 }, (_, j) => `$${i * 5 + (j + 1)}`).join(", ")})`).join(",\n")}`
+      const values = authors.flatMap(a => [a.name, a.title, a.books_count, a.users_count, a.id])
+
+      await instance.query(query, values)
+
+      res.json({ SUCCESS: "Insert successful!" })
+    } catch (err: any) {
+      res.status(500).json({ ERROR: err.message })
+    }
   } catch (err: any) {
-    res.status(500).json({ ERROR: err.message })
+    console.error(err.message)
   }
+  
 }
 
 const fetchPublishers = async (_: Request, res: Response) => {
   try {
-    const { data } = await fetchGraphQL(operationsDoc, "dataPublishers", {})
-    res.json({ data })
+    const instance = await pool.connect()
+
+    try {
+      const { data } = await fetchGraphQL(operationsDoc, "dataPublishers", {})
+      if (!data.publishers)
+        throw new Error("Publishers array was not found")
+
+      const publishers: Array<{ id: number, name: string }> = data.publishers
+      
+      const query = `INSERT INTO publishers(name, id) VALUES ${publishers.map((_, i) => `($${i * 2 + 1}, $${i * 2 + 2})`).join(", ")}`
+      const values = publishers.flatMap(p => [p.name, p.id])
+      
+      await pool.query(query, values)
+      res.json({ SUCCESS: "Insert successful!" })
+      
+    } catch (err: any) {
+      res.status(500).json({ ERROR: err.message })
+    } finally {
+      instance.release()
+    }
   } catch (err: any) {
-    res.status(500).json({ ERROR: err.message })
+    console.error(err.message)
   }
+  
 }
 
 const fetchReviews = async (_: Request, res: Response) => {
